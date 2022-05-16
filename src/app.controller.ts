4c262bf75c4b5bef61d384240f56a7695cff5891
ploopple <60466939+ploopple@users.noreply.github.com>
2022-05-16 14:24:57 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get('/api')
+  @Get('/')
   getHello() {
     const msg = `${process.env.MSG}`;
     return { msg };
