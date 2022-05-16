789a1e345d3e2b78ce64528e5c47a72a516bd931
ploopple <60466939+ploopple@users.noreply.github.com>
2022-05-16 14:37:30 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,13 @@ export class AppController {
   @Get('/')
   getHello() {
     const msg = `${process.env.MSG}`;
-    return { msg };
+    return return `
+    <html>
+    <head>
+    <title>one</title>
+    </head>
+    <body><h1>Hello</h1></body>
+    </html>
+    `;
   }
 }
